ba530cc8d5ea4fdd790ef67d816c3f1adc253f17
Samuel <sam055@mac.com>
2021-08-06 15:09:35 +0100

    Update sensitivity_results.jl

--- sensitivity_analysis/sensitivity_results.jl
+++ sensitivity_analysis/sensitivity_results.jl
@@ -5,7 +5,7 @@ using DynamicHMC.Diagnostics,MCMCDiagnostics,MCMCChains,ForwardDiff,LogExpFuncti
 using Plots.PlotMeasures,LogExpFunctions
 using CountTimeSeries
 import KenyaCoVSD
-
+include("inference_variable_transmissibility.jl");
 ## Gather the fits
 waning_immunity_scenarios = [(σ = 0.0,ω = 1/180,scenario_name = "_no_waning_immunity"),
               (σ = 0.5,ω = 1/180,scenario_name = "_med_susceptibility"),
@@ -34,4 +34,10 @@ push!(all_scenarios,(σ = 0.16,ω = 1/365,ι = 1.0, scenario_name = "_slow_rever
 @load("sensitivity_analysis/nairobi_fits/Nairobi_model_slow_reversion_to_full_susceptibility.jld2")
 push!(all_scenarios,(σ = 1.0,ω = 1/(5*365),ι = 1.0, scenario_name = "_slow_reversion_to_full_susceptibility",fit= model,DIC = KenyaCoVSD.modeldic(model)))
 @load("sensitivity_analysis/nairobi_fits/Nairobi_modelreduced_inf.jld2")
-push!(all_scenarios,(σ = 1.0,ω = 1/(5*365),ι = 1.0, scenario_name = "_slow_reversion_to_full_susceptibility",fit= model,DIC = KenyaCoVSD.modeldic(model)))
+push!(all_scenarios,(σ = 0.16,ω = 1/180,ι = 0.5, scenario_name = "reduced_inf",fit= model,DIC = KenyaCoVSD.modeldic(model)))
+@load("sensitivity_analysis/nairobi_fits/Nairobi_model_med_susceptibility_reduced_inf.jld2")
+push!(all_scenarios,(σ = 0.5,ω = 1/180,ι = 0.5,scenario_name = "_med_susceptibility_reduced_inf",fit= model,DIC = KenyaCoVSD.modeldic(model)))
+@load("sensitivity_analysis/nairobi_fits/Nairobi_model_full_susceptibility_reduced_inf.jld2")
+push!(all_scenarios,(σ = 1.0,ω = 1/180,ι = 0.5,scenario_name = "_full_susceptibility_reduced_inf",fit= model,DIC = KenyaCoVSD.modeldic(model)))
+@load("sensitivity_analysis/nairobi_fits/Nairobi_model_full_susceptibility_fast_wane_reduced_inf.jld2")
+push!(all_scenarios,(σ = 1.0,ω = 1/90,ι = 0.5,scenario_name = "_full_susceptibility_fast_wane_reduced_inf",fit= model,DIC = KenyaCoVSD.modeldic(model)))
